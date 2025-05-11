465c84f329a5e3e9434d70078d92c5aff74ac691
Egor <lobanow.egor@outlook.com>
2025-05-11 17:35:38 +0300

    Сделал взаимодействие с БД

--- database/database.py
+++ database/database.py
@@ -9,6 +9,7 @@ def create_database():
         cursor.execute('''
         CREATE TABLE IF NOT EXISTS Users(
         id INTEGER PRIMARY KEY,
+        telegram_id INTEGER PRIMARY KEY,
         isAdmin BOOLEAN NOT NULL
         )
         ''')
@@ -31,15 +32,47 @@ def create_database():
         ''')
 
         cursor.execute('''
-        CREATE TABLE IF NOT EXISTS TodayMenu(
-        id INTEGER PRIMARY KEY,
-        name VARCHAR(256) UNIQUE NOT NULL,
-        text VARCHAR(256) NOT NULL
-        )
-        ''')
+                CREATE INDEX IF NOT EXISTS idx_food ON Food(name)
+                ''')
+        cursor.execute('''
+                CREATE INDEX IF NOT EXISTS idx_user_food ON Favorites(user_id)
+                ''')
+
 
         conn.commit()
 
+async def insert_user(telegram_id: int, isAdmin: bool):
+    with closing(sqlite3.connect('database.db')) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''INSERT OR IGNORE INTO Users (telegram_id, isAdmin) VALUES (?, ?)''', (telegram_id, isAdmin))
+        conn.commit()
+
+
+async def insert_food(name, text):
+    with closing(sqlite3.connect('database.db')) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+        INSERT OR IGNORE INTO Food(name, text) VALUES (?, ?)
+        ''', (name, text))
+        conn.comit()
+
+async def add_favorite(telegram_id, food_name):
+    with closing(sqlite3.connect('database.db')) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+        SELECT id FROM Users WHERE telegram_id = ?
+        ''', (telegram_id, ))
+        user_id = cursor.fetchone()[0]
+        cursor.execute('''
+                SELECT id FROM Food WHERE name = ?
+                ''', (food_name,))
+        food_id = cursor.fetchone()[0]
+
+        cursor.execute('''
+        INSERT OR IGNORE INTO Favorite(user_id, food_id) VALUES (?, ?)
+        ''', (user_id, food_id))
+
+        conn.commit()
 
 if __name__ == '__main__':
     create_database()
